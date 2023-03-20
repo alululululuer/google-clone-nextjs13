e50718658b2c2d0d5958bd2058b687946d905523
alululululuer <alululululuer@163.com>
2023-03-20 23:51:19 +0800

    install next js and create the first template

---
+++ src/app/layout.jsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Google Clone Next js 13",
+  description: "Google clone created by Next js 13",
+  icons: {
+    icon: "/favicon.svg",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
